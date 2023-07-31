a08079ac148e3f8f9b41878ae6cda2125766c4d3
ashwini.k <ashwinikadganji@gmail.com>
2023-07-31 22:45:43 +0530

    Create 06_while_assignment02.js

---
+++ 06_while_assignment02.js
@@ -0,0 +1,61 @@
+console.log(`-----------------question 1---------------------`);
+console.log(`WAP to print numbers from 5 to 15 by incrementing 1`);
+//WAP to print numbers from 5 to 15 by incrementing 1
+var i = 5;
+while (i <= 15) {
+    console.log(`${i}`);
+    i++;
+}
+
+console.log(`-----------------question 2---------------------`);
+console.log(`WAP to print numbers from 50 to 40 by decrementing by 1`);
+// WAP to print numbers from 50 to 40 by decrementing by 1
+var i = 50;
+while (i >= 40) {
+    console.log(`${i}`);
+    i--;
+}
+console.log(`-----------------question 3---------------------`);
+console.log(`WAP to find  first 15 odd numbers`);
+// WAP to find  first 15 odd numbers
+var i = 1;
+while (i <= 30) {
+    console.log(`${i}`);
+    i = i + 2;
+}
+
+console.log(`-----------------question 4---------------------`);
+console.log(`WAP to find first 10 even numbers`);
+// WAP to find first 10 even numbers
+var i = 0;
+while (i < 20) {
+    console.log(`${i}`);
+    i = i + 2;
+}
+
+console.log(`-----------------question 5---------------------`);
+console.log(`WAP to print table of 5 like -> 5 10 15 ..... 50`);
+//WAP to print table of 5 like -> 5 10 15 ..... 50
+var i = 5;
+while (i <= 50) {
+    console.log(`${i}`);
+    i = i + 5;
+}
+
+console.log(`-----------------question 6---------------------`);
+console.log(`WAP to print table of 10 like -> 10 20 30 40....100`);
+//WAP to print table of 10 like -> 10 20 30 40....100
+var i = 10;
+while (i <= 100) {
+    console.log(`${i}`);
+    i = i + 10;
+}
+
+console.log(`-----------------question 7---------------------`);
+console.log(`WAP to print table of 10 in reverse oreder like -> 100 90 80 .... 10 `);
+//WAP to print table of 10 in reverse oreder like -> 100 90 80 .... 10 
+var i = 100;
+while (i >= 10) {
+    console.log(`${i}`);
+    i = i - 10;
+}
