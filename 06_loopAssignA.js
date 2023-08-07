0dd14bcf7467b94e697645c2c327de8c5c268cd3
ashwini.k <ashwinikadganji@gmail.com>
2023-08-07 20:31:32 +0530

    vowels count , sum of cube, odd positions characters
    vowels count , sum of cube, odd positions characters

---
+++ 06_loopAssignA.js
@@ -0,0 +1,58 @@
+console.log(`-------------step 1----------`);
+// WAP count the total number vowels including small and capital vowels for usinf while loop - > 'i am very good It developer'
+//  console.log( `str = I am very good IT Developer`);
+
+// function vowelsCount(str) {
+//     let count = 0;
+//     for (let index = 0; index < str.length; index++) {
+//         let char = str.charAt(index);
+        
+//     while (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U') {
+         
+//         console.log(char);
+//         count = count+1;
+//    }
+//  }
+//  console.log(`Total number of vowels is: ${count}`);
+// }
+// vowelsCount(`I am very good IT Developer`);
+
+
+console.log(`-------------step 2----------`);
+//Write a function to get the sum of cube of numbers from 1 to 5
+function cubeSum(cube) {
+    let sumOfCube = 0;
+    for (let index = 1; index <= 5; index++) {
+        sumOfCube = sumOfCube + index * index * index;   
+    }
+    console.log(`the sum of cube of numbers from 1 to 5: ${sumOfCube}`);
+}
+cubeSum();
+
+console.log(`-------------step 3----------`);
+// str1 = 'hard work always pays back ', str2 = 'soon i will be angular IT champ'
+// function oddPodsitionChars() with one org
+// log only odd positioned chars on console and do not consider empty spaces
+// invoke or call this function for above string1 and string2 separately
+
+function oddPositionChars(str)
+ {
+    let position ='';
+    for (let index = 0; index <= str.length;index++) {
+        const char = str.charAt(index);
+     // log only odd positioned chars on console and do not consider empty spaces
+     if (index % 2 != 0 && char != ' ') 
+     {
+        position = position.concat(char);
+     }   
+        
+    }
+    console.log(`${position}`);
+}
+oddPositionChars('Hard work always pays back');
+oddPositionChars('Soon i will be angular IT champ')
+
+
+
+
+
--- 07_vowels.js
+++ 07_vowels.js
@@ -1,22 +1,27 @@
 function vowelCount(str) {
+    let count = 0;
         for (let index = 0; index < str.length; index++) {
             let char = str.charAt(index);
 
     if (char == 'a' || char == 'A' || char == 'e' || char == 'E' || char == 'i' || char == 'I' || char == 'o' || char == 'O' || char == 'u' || char == 'U') {
         console.log(char);
+        count = count+1;
     }   
 }
+console.log(`Total number of vowels is : ${count}`);
 }
 vowelCount(`I am a Angular Developer`);
 
 console.log(`----------------------------------------------`);
 
-function oddPositionedChars(str) {
-    for (let index = 0; index < str.length; index++) {
-        const char = str.charAt(index);
-        // Log only odd positioned chars on console and do not consider empty spaces
-    }
+console.log(`---------------------------`);
 
+//WAP to get sum of number from 1 to 20
+let sum = function(){
+   let totalSum = 0;
+   for (let index = 1; index <= 20; index++) {
+     totalSum = totalSum + index ;
+   }
+   console.log(`sum of number from 1 to 20: ${totalSum}`);
 }
-oddPositionedChars("hello");
-oddPositionedChars("test");
+sum();
